ed9a2013711f1c9c4bbb49fc753a47e19938d173
Nail Achmedzhanov <achmedzhanov@gmail.com>
2019-03-18 19:04:30 +0400

    more animations

--- scripts/main.js
+++ scripts/main.js
@@ -54,6 +54,12 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
         return this;
       }
     }, {
+      key: "textContent",
+      value: function textContent(value) {
+        this._el.textContent = value;
+        return this;
+      }
+    }, {
       key: "style",
       value: function style(name, value) {
         this._el.style[name] = value;
@@ -83,12 +89,20 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
   }();
 
   var xCoef = 1,
-      yCoef = 1,
       xStep = 20;
+
+  var translateY = function translateY(y) {
+    return y;
+  };
 
   function createChart(options) {
     var el = options.el,
         chartData = options.chartData;
+    var sizes = options.sizes;
+    sizes = sizes || {
+      width: 500,
+      height: 250
+    };
     var colors = chartData.colors,
         columns = chartData.columns,
         types = chartData.types;
@@ -146,6 +160,7 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
     };
 
     var getBounds = function getBounds(s) {
+      // const ty = !!s;
       s = s || {
         disabled: {},
         visibleRange: {
@@ -166,14 +181,19 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
       };
       var yMax = Math.max.apply(Math, _toConsumableArray(visibleLines.map(function (l) {
         return Math.max.apply(Math, _toConsumableArray(l.slice(visibleIndexRange.from, visibleIndexRange.to + 1)));
-      })));
-      var yMin = Math.min.apply(Math, _toConsumableArray(visibleLines.map(function (l) {
-        return Math.min.apply(Math, _toConsumableArray(l.slice(visibleIndexRange.from, visibleIndexRange.to + 1)));
-      })));
-      var xSize = (xColumn.length - 1) * xStep;
+      }))); //let yMin = Math.min(...visibleLines.map(l => Math.min(...l.slice(visibleIndexRange.from, visibleIndexRange.to + 1))));
+
+      var xSize = (xColumn.length - 2) * xStep;
       var xMin = xSize * s.visibleRange.from / 100;
-      var xMax = xSize * s.visibleRange.to / 100;
-      return [xMin, xMax, yMin, yMax];
+      var xMax = xSize * s.visibleRange.to / 100; // if(ty) {
+      //     const yMaxOld = yMax;
+      //     yMax = translateY(yMin);
+      //     yMin = translateY(yMaxOld);
+      // }
+
+      return [xMin, xMax, 0
+      /*yMin*/
+      , yMax];
     };
 
     var init = function init() {
@@ -181,38 +201,57 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
       state.fullBounds = fullBounds;
 
       var _fullBounds = _slicedToArray(fullBounds, 4),
-          xMin = _fullBounds[0],
           xMax = _fullBounds[1],
           yMin = _fullBounds[2],
           yMax = _fullBounds[3];
 
-      var width = xMax - xMin,
-          height = yMax - yMin;
-      var aspectRatio = 2 / 1;
-      var yScale = 1 / (height / width) / aspectRatio;
-      var viewBoxPadding = 50;
-      var viewBox = [xMin * xCoef - viewBoxPadding, yMin * yScale - viewBoxPadding, xMax * xCoef + viewBoxPadding, yMax * yScale + viewBoxPadding];
-      var svgEl = createSVG('svg').style('width', '100%').style('height', '100%').attr('viewBox', "".concat(viewBox[0], " ").concat(viewBox[1], " ").concat(viewBox[2], " ").concat(viewBox[3])).attr('zoom', 1); // TODO chekc view port when xmin really more  0
-      // TODO use clip?
-      // TODO need to scle coordinates to minimive viewposrt of svg?
+      state.sizes = sizes;
+      var xScale = sizes.width / xMax;
+      var yScale = sizes.height / yMax;
+      var viewBox = [0, 0, sizes.width, sizes.height];
 
-      var initalTransform = "matrix(1, 0, 0, ".concat(yScale, ", 0, 0)"); //const initalTransform = `matrix(1, 0, 0, 1, 0, 0)`;
+      translateY = function translateY(y) {
+        return -(y - yMin) + yMax;
+      }
+      /* -yMin  */
+      ;
 
+      var svgEl = createSVG('svg').style('width', sizes.width).style('height', sizes.height).attr('viewBox', "".concat(viewBox[0], " ").concat(viewBox[1], " ").concat(viewBox[2], " ").concat(viewBox[3])).attr('zoom', 1);
+      var initalTransform = "matrix(".concat(xScale, ", 0, 0, ").concat(yScale, ", 0, 0)");
       var lc = 5;
-      var hGridLines = createSVG('g').addClass('animate-transform').style('vector-effect', 'non-scaling-stroke').style('stroke', 'gray').style('fill', 'none').style('vector-effect', 'non-scaling-stroke').attr('transform', initalTransform).appendTo(svgEl);
+      var hGridLines = []; //const hGridTexts = [];
 
-      for (var i = 0; i <= lc; i++) {
-        var y = yMin + (yMax - yMin) / lc * i;
-        createSVG('path').style('stroke-width', '1px').style('vector-effect', 'non-scaling-stroke').attr('d', 'M0 ' + y + ' L' + xMax + ' ' + y).appendTo(hGridLines);
+      var hGridLinesG = createSVG('g').addClass('animate-transform').style('vector-effect', 'non-scaling-stroke').appendTo(svgEl);
+
+      for (var i = 0; i <= lc; i++) {// let yGraph = (yMax - yMin) / lc * i;
+        // let y = yMin + (yMax - yMin) / lc * i * yScale;
+        // const lEl = createSVG('path')
+        // .style('stroke-width', '1px')
+        // .style('stroke', 'lightblue')
+        // .style('fill', 'none')
+        // .style('vector-effect', 'non-scaling-stroke')
+        // .attr('d',  'M0 ' + y + ' L' + (xMax * xScale) +' ' + y)
+        // .appendTo(hGridLinesG);
+        // hGridLines.push(lEl);
+        // const tEl = createSVG('text')
+        // .attr('x',  '0')
+        // .attr('y',  y - 5)
+        // // .attr('font-family', ' "Helvetica Neue", Helvetica, Arial, sans-serif')
+        // .attr('font-family', 'sans-serif')
+        // .attr('font-size', '14')
+        // .attr('fill', 'gray')
+        // .textContent('' + translateY(yGraph))
+        // .appendTo(hGridLinesG);
+        // hGridTexts.push(tEl);
       }
 
+      state.elements.hGridLinesG = hGridLinesG;
       state.elements.hGridLines = hGridLines;
       var columnIds = Object.keys(types); // ==>> TODO !!!! invert coordinates !!!!
 
       var lineElements = {};
-      var linesGC = createSVG('g').addClass('animate-transform') //.attr('transform', initalTransform)
-      .appendTo(svgEl);
-      var linesG = createSVG('g').attr('transform', initalTransform).appendTo(linesGC);
+      var linesGC = createSVG('g').addClass('animate-transform').attr('transform', initalTransform).appendTo(svgEl);
+      var linesG = createSVG('g').appendTo(linesGC);
 
       for (var columnIndex = 0; columnIndex < columnIds.length; columnIndex++) {
         var columnName = columnIds[columnIndex],
@@ -227,7 +266,7 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
             d += pIdx == 1 ? 'M' : 'L';
             d += (pIdx - 1) * xStep * xCoef;
             d += ' ';
-            d += c[pIdx] * yCoef;
+            d += translateY(c[pIdx]);
           }
 
           var p = createSVG('path').attr('d', d).style('stroke', color).style('stroke-width', '2px').style('vector-effect', 'non-scaling-stroke').style('fill', 'none').appendTo(linesG);
@@ -263,30 +302,101 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
         from: 0,
         to: 100
       };
+      if (state.visibleRange.from == range.from && state.visibleRange.to == range.to) return;
+      var prevBounds = getBounds(state);
       state.visibleRange = range;
-      var W = state.fullBounds[1] - state.fullBounds[0];
+      var fbyMax = state.fullBounds[3];
+      var newBounds = getBounds(state);
 
-      var _getBounds = getBounds(state),
-          _getBounds2 = _slicedToArray(_getBounds, 4),
-          xMin = _getBounds2[0],
-          xMax = _getBounds2[1],
-          yMin = _getBounds2[2],
-          yMax = _getBounds2[3];
+      var _newBounds = _slicedToArray(newBounds, 4),
+          xMin = _newBounds[0],
+          xMax = _newBounds[1],
+          yMin = _newBounds[2],
+          yMax = _newBounds[3];
 
-      var w = xMax - xMin,
-          h = yMax - yMin;
-      var aspectRatio = 2 / 1;
-      var xScale = W / w;
-      var yScale = 1 / (h / W) / aspectRatio;
-      var dx = -xMin,
-          dy = -yMin;
-      var verticalTransform = "matrix(1,0,0,1,0,".concat(dy, ") matrix(1,0,0,").concat(yScale, ",0,0)");
+      var xScale = state.sizes.width / xMax;
+      var yScale = state.sizes.height / yMax;
+      var dx = -xMin
+      /*, dy = 0 /*-yMin*/
+      ;
+      var verticalTransform = "matrix(1,0,0,1,0,".concat(yMax * yScale, ") matrix(1,0,0,").concat(yScale, ",0,0) matrix(1,0,0,1,0,").concat(-fbyMax, ")");
       var horizontalTransform = "matrix(1,0,0,1,".concat(dx, ",0) matrix(").concat(xScale, ",0,0,1,0,0)");
       state.elements.linesGC.attr('transform', verticalTransform);
-      state.elements.linesG.attr('transform', horizontalTransform);
-      state.elements.hGridLines.attr('transform', verticalTransform); // for(let l of Object.values(state.elements.linesElements)) {
-      //     l.setAttribute('transform', t);
-      // }
+      state.elements.linesG.attr('transform', horizontalTransform); //state.elements.hGridLinesG.attr('transform', verticalTransform);
+
+      if (prevBounds[2] !== yMin || prevBounds[3] !== yMax) {
+        // update y axis lines
+        //state.elements.hGridLinesG.attr('transform', 'none');
+        var lc = 5;
+        var newLines = calcYAxis(yScale, prevBounds, lc); // console.log('newLines', newLines);
+
+        var gridElements = createYGridLines(newLines, function (el) {
+          return el.style('opacity', '0.1');
+        });
+        var movedLines = calcYAxis(yScale, newBounds, lc); // console.log('movedLines', movedLines);
+
+        setTimeout(function () {
+          updateYGridLines(gridElements, movedLines, function (el) {
+            return el.style('opacity', '1');
+          });
+        }, 0);
+      }
+    };
+
+    var calcYAxis = function calcYAxis(yScale, bounds, lc) {
+      var _bounds = _slicedToArray(bounds, 4),
+          yMin = _bounds[2],
+          yMax = _bounds[3];
+
+      var lines = [];
+
+      for (var i = 0; i <= lc; i++) {
+        var yGraph = (yMax - yMin) / lc * i;
+        var y = yMin + (yMax - yMin) / lc * i * yScale;
+        lines.push({
+          y: y,
+          yGraph: yGraph,
+          text: translateY(yGraph)
+        });
+      }
+
+      return lines;
+    };
+
+    var createYGridLines = function createYGridLines(lines, cb) {
+      var hGridLines = [];
+      var hGridTexts = [];
+      var hGridLinesG = state.elements.hGridLinesG;
+      var lc = lines.length;
+
+      for (var i = 0; i < lc; i++) {
+        var y = lines[i].y;
+        var lEl = createSVG('path').style('stroke-width', '1px').style('stroke', 'lightblue').style('fill', 'none').style('vector-effect', 'non-scaling-stroke').attr('d', 'M0 ' + y + ' L' + state.sizes.width + ' ' + y).addClass('animate-transform-opacity').appendTo(hGridLinesG);
+        hGridLines.push(lEl);
+        cb(lEl);
+        var tEl = createSVG('text').attr('x', '0').attr('y', y - 5).attr('font-family', 'sans-serif').attr('font-size', '14').attr('fill', 'gray').textContent('' + lines[i].text).addClass('animate-transform-opacity').appendTo(hGridLinesG);
+        hGridTexts.push(tEl);
+        cb(tEl);
+      }
+
+      return {
+        hGridLines: hGridLines,
+        hGridTexts: hGridTexts
+      };
+    };
+
+    var updateYGridLines = function updateYGridLines(gridElements, lines, cb) {
+      var hGridLines = gridElements.hGridLines,
+          hGridTexts = gridElements.hGridTexts;
+      var lc = lines.length;
+
+      for (var i = 0; i < lc; i++) {
+        var y = lines[i].y;
+        hGridLines[i].attr('d', 'M0 ' + y + ' L' + state.sizes.width + ' ' + y);
+        cb(hGridLines[i]);
+        hGridTexts[i].textContent('' + lines[i].text).attr('x', '0').attr('y', y - 5);
+        cb(hGridTexts[i]);
+      }
     };
 
     init();
@@ -306,6 +416,21 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 "use strict";
 
 window.globalChartsData = [{
+  'columns': [['x', 1542412800000, 1542499200000, 1542585600000, 1542672000000, 1542758400000, 1542844800000, 1542931200000, 1543017600000, 1543104000000, 1543190400000, 1543276800000], ['y0', 10, 5, 15, 40, 50, 50, 50, 65, 100, 75, 70], ['y1', 10, 5, 15, 40, 50, 50, 50, 65, 100, 75, 70]],
+  'types': {
+    'y0': 'line',
+    'y1': 'line',
+    'x': 'x'
+  },
+  'names': {
+    'y0': '#0',
+    'y1': '#1'
+  },
+  'colors': {
+    'y0': '#3DC23F',
+    'y1': '#F34C44'
+  }
+}, {
   'columns': [['x', 1542412800000, 1542499200000, 1542585600000, 1542672000000, 1542758400000, 1542844800000, 1542931200000, 1543017600000, 1543104000000, 1543190400000, 1543276800000, 1543363200000, 1543449600000, 1543536000000, 1543622400000, 1543708800000, 1543795200000, 1543881600000, 1543968000000, 1544054400000, 1544140800000, 1544227200000, 1544313600000, 1544400000000, 1544486400000, 1544572800000, 1544659200000, 1544745600000, 1544832000000, 1544918400000, 1545004800000, 1545091200000, 1545177600000, 1545264000000, 1545350400000, 1545436800000, 1545523200000, 1545609600000, 1545696000000, 1545782400000, 1545868800000, 1545955200000, 1546041600000, 1546128000000, 1546214400000, 1546300800000, 1546387200000, 1546473600000, 1546560000000, 1546646400000, 1546732800000, 1546819200000, 1546905600000, 1546992000000, 1547078400000, 1547164800000, 1547251200000, 1547337600000, 1547424000000, 1547510400000, 1547596800000, 1547683200000, 1547769600000, 1547856000000, 1547942400000, 1548028800000, 1548115200000, 1548201600000, 1548288000000, 1548374400000, 1548460800000, 1548547200000, 1548633600000, 1548720000000, 1548806400000, 1548892800000, 1548979200000, 1549065600000, 1549152000000, 1549238400000, 1549324800000, 1549411200000, 1549497600000, 1549584000000, 1549670400000, 1549756800000, 1549843200000, 1549929600000, 1550016000000, 1550102400000, 1550188800000, 1550275200000, 1550361600000, 1550448000000, 1550534400000, 1550620800000, 1550707200000, 1550793600000, 1550880000000, 1550966400000, 1551052800000, 1551139200000, 1551225600000, 1551312000000, 1551398400000, 1551484800000, 1551571200000, 1551657600000, 1551744000000, 1551830400000, 1551916800000, 1552003200000], ['y0', 37, 20, 32, 39, 32, 35, 19, 65, 36, 62, 113, 69, 120, 60, 51, 49, 71, 122, 149, 69, 57, 21, 33, 55, 92, 62, 47, 50, 56, 116, 63, 60, 55, 65, 76, 33, 45, 64, 54, 81, 180, 123, 106, 37, 60, 70, 46, 68, 46, 51, 33, 57, 75, 70, 95, 70, 50, 68, 63, 66, 53, 38, 52, 109, 121, 53, 36, 71, 96, 55, 58, 29, 31, 55, 52, 44, 126, 191, 73, 87, 255, 278, 219, 170, 129, 125, 126, 84, 65, 53, 154, 57, 71, 64, 75, 72, 39, 47, 52, 73, 89, 156, 86, 105, 88, 45, 33, 56, 142, 124, 114, 64], ['y1', 22, 12, 30, 40, 33, 23, 18, 41, 45, 69, 57, 61, 70, 47, 31, 34, 40, 55, 27, 57, 48, 32, 40, 49, 54, 49, 34, 51, 51, 51, 66, 51, 94, 60, 64, 28, 44, 96, 49, 73, 30, 88, 63, 42, 56, 67, 52, 67, 35, 61, 40, 55, 63, 61, 105, 59, 51, 76, 63, 57, 47, 56, 51, 98, 103, 62, 54, 104, 48, 41, 41, 37, 30, 28, 26, 37, 65, 86, 70, 81, 54, 74, 70, 50, 74, 79, 85, 62, 36, 46, 68, 43, 66, 50, 28, 66, 39, 23, 63, 74, 83, 66, 40, 60, 29, 36, 27, 54, 89, 50, 73, 52]],
   'types': {
     'y0': 'line',
@@ -396,7 +521,7 @@ function start() {
   onDocumentReady(function () {
     var data = globalChartsData;
 
-    for (var i = 0; i < 2; i++) {
+    for (var i = 0; i < 5; i++) {
       createChart({
         el: document.getElementById('chart' + i),
         chartData: data[i]
